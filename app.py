8261818b0a7f3e7d01591e4cf90a9ada8e5fc06b
jeremyy <ing.jeremy.quiel@gmail.com>
2026-04-25 13:53:21 -0600

    registro libros y prestamos

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import *
 from conexion.Conn import Conn
 import os
 from dotenv import load_dotenv
+from datetime import date, timedelta
 
 load_dotenv()
 
@@ -16,6 +17,9 @@ app.secret_key = SECRET_KEY
 def index():
     session.pop('logueado', None)
     session.pop('usuario', None)
+    session.pop('mensaje_registro_usuario', None)
+    session.pop('mensaje_registro_libro', None)
+    session.pop('mensaje_prestamo', None)
 
     session.setdefault('mensaje_login', "") # Crea la session si no existe
 
@@ -28,9 +32,10 @@ def registro_usuarios():
 
 @app.route('/registro/libros')
 def registro_libros():
-    return render_template('registro_libros.html')
+    session.setdefault('mensaje_registro_libro', "")
+    return render_template('registro_libros.html', mensaje = session.get('mensaje_registro_libro'))
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 def login():
     if request.method == 'POST':
         correo = request.form.get('correo')
@@ -78,9 +83,23 @@ def usuario_correo():
 
 @app.route('/usuario/prestamo')
 def usuario_prestamo():
-    return render_template('usuario_prestamo.html')
+    if not session.get('logueado'):
+        return redirect(url_for('index'))
+    
+    session.setdefault('mensaje_prestamo', "")
 
-@app.route('/crear/usuario', methods=['GET', 'POST'])
+    try:
+        CURSOR.execute("SELECT id_libro, nombre_libro, nombre_autor, apellido_autor, precio_base FROM libro WHERE estado = 1")
+        lista = CURSOR.fetchall()
+    except Exception as e:
+        return render_template('usuario_prestamo.html', mensaje = session.get('mensaje_prestamo'), codigo_mensaje = "error", e = e)
+    
+    if not lista:
+        return render_template('usuario_prestamo.html', mensaje = session.get('mensaje_prestamo'), codigo_mensaje = "vacia")
+    
+    return render_template('usuario_prestamo.html', mensaje = session.get('mensaje_prestamo'), codigo_mensaje = "lista", lista = lista)
+
+@app.route('/crear/usuario', methods=['POST'])
 def crear_usuario():
     if request.method == 'POST':
         nombre = request.form.get('nombre')
@@ -136,8 +155,87 @@ def crear_usuario():
     session['mensaje_registro_usuario'] = "ERROR DE METODO: metodo no es POST"
     return redirect(url_for('index'))
 
-@app.route('/crear/libros', methods=['GET', 'POST'])
+@app.route('/crear/libros', methods=['POST'])
 def crear_libros():
+    if request.method == 'POST':
+        titulo = request.form.get('titulo')
+        nombre = request.form.get('nombre')
+        apellido = request.form.get('apellido')
+        precio = request.form.get('precio')
+
+        if not (titulo and nombre and precio):
+            session['mensaje_registro_libro'] = "ERROR: Llene todos los campos obligatorios"
+            return redirect(url_for('registro_libros'))
+        
+        try:
+            CURSOR.execute("SELECT * FROM libro WHERE nombre_libro = %s and nombre_autor = %s",(titulo, nombre))
+
+            libro = CURSOR.fetchone()
+
+            if libro:
+                session['mensaje_registro_libro'] = f"{titulo} de {nombre} {apellido} ya esta registrado"
+                return redirect(url_for('registro_libro'))
+            
+        except Exception as e:
+            session['mensaje_registro_libro'] = f"ERORR BUSQUEDA: {e}"
+            return redirect(url_for('registro_libros'))
+        
+
+        try:
+            CURSOR.execute('INSERT INTO libro (nombre_libro, nombre_autor, apellido_autor, precio_base) VALUES (%s, %s, %s, %s)',(titulo, nombre, apellido, precio))
+
+            MYSQL.commit()
+
+            session['mensaje_registro_libro'] = "Se registro el libro correctamente"
+            return redirect(url_for('registro_libros'))
+        except Exception as e:
+            MYSQL.rollback()
+
+            session['mensaje_registro_libro'] = f"ERRO INSERTO: {e}"
+            return redirect(url_for('registro_libros'))
+
+    return redirect(url_for('registro_libros'))
+
+@app.route('/crear/prestamo', methods = ['POST'])
+def crear_prestamo():
+    if request.method == 'POST' and session.get('logueado'):
+        id_libro = request.form.get('id_libro')
+
+        if not id_libro:
+            session['mensaje_prestamo'] = "ERROR: Llene el campo"
+            return redirect(url_for('usuario_prestamo'))
+        
+        try:
+            CURSOR.execute("SELECT * FROM libro WHERE id_libro = %s",(id_libro))
+
+            libro = CURSOR.fetchone()
+
+            if not libro:
+                session['mensaje_prestamo'] = f"Este ID de libro no esta registrado"
+                return redirect(url_for('usuario_prestamo'))
+            
+        except Exception as e:
+            session['mensaje_prestamo'] = f"ERORR BUSQUEDA: {e}"
+            return redirect(url_for('usuario_prestamo'))
+        
+        usuario = session.get('usuario')
+
+        fecha_actual = date.today()
+        fecha_fin = fecha_actual + timedelta(days=30)
+
+        try:
+            CURSOR.execute('INSERT INTO prestamo (id_usuario, id_libro, fecha_fin) VALUES (%s, %s, %s)',(usuario['id'], id_libro, fecha_fin))
+            CURSOR.execute('UPDATE libro SET estado = 0 WHERE id_libro = %s', (id_libro))
+            MYSQL.commit()
+
+            session['mensaje_prestamo'] = "Prestamo creado correctamente"
+            return redirect(url_for('usuario_prestamo'))
+        except Exception as e:
+            MYSQL.rollback()
+
+            session['mensaje_prestamo'] = f"ERRO INSERTO: {e}"
+            return redirect(url_for('usuario_prestamo'))
+
     return redirect(url_for('index'))
 
 @app.route('/logout')
@@ -146,6 +244,7 @@ def logout():
 
 @app.route('/volver')
 def volver():
+    session.pop('mensaje_login', None)
     return redirect(url_for('index'))
 
     
